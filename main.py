ee3acdca755514ab80cb37f49b0bf26c2ec3fd85
SoggyCereal <PhillipsJo860@northwested.org>
2024-11-04 10:07:39 -0500

    Added sum looping

---
+++ main.py
@@ -0,0 +1,12 @@
+# Joshua Phillips
+# 11/1/24
+# Pyhton for loops practice
+
+# EXAMPLE #4-3
+# for i in range(1, 21, 1):
+#     print(i)
+
+# EXAMPLE #4-6
+# for i in range(1, 20, 2):
+#     print(i)
+
